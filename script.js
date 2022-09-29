a4053aa6b6ea84f4ac991efddc7f232b92075c7f
Garenkov <vladgarenkov@gmail.com>
2022-09-29 03:33:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function Tamagochi(){
     tmg.className = 'tmg';
     document.querySelector('.container').append(tmg);
 
-    const hpe = document.createElement('div');
-    hpe.className = 'hp';
-    tmg.append(hpe);
+    const hp = document.createElement('div');
+    hp.className = 'hp';
+    tmg.append(hp);
 
     const face = document.createElement('img');
     face.className = 'face';
@@ -23,9 +23,9 @@ function Tamagochi(){
     this.killer = function(){
         if(this.health > 0){
             this.health--
-            hpe.style.width = this.health + 'px';
+            hp.style.width = this.health + 'px';
         } else {
-            hpe.style.opacity = 0;
+            hp.style.opacity = 0;
             face.src = './img/RIP.png';
             tmg.removeEventListener('click', () => this.save());
 
@@ -37,7 +37,7 @@ function Tamagochi(){
             }, 1000);
             lives++;
             document.querySelector('.count').innerHTML = 'Умерло: ' + lives;
-            clearInterval(time);
+            clearInterval(timer);
         }
     };
     this.save = function(){
@@ -48,10 +48,10 @@ function Tamagochi(){
         }
     };
     this.face = function(){
-        const faceArr = ['./img/tmg1.png', './img/tmg3.png', './img/tmg4.png', './img/tmg5.png', './img/tmg6.png', './img/tmg7.png' ];
+        const faceArr = ['./img/tmg1.png', './img/tmg3.png', './img/tmg4.png', './img/tmg5.png', './img/tmg6.png', './img/tmg7.png'];
         face.src = faceArr[Math.floor(Math.random() * faceArr.length)];
     }
-    const time = setInterval(() => this.killer(), 100);
+    const timer = setInterval(() => this.killer(), 100);
 }
 
 function createTmg(){
